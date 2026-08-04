7ae86fb49dd86609b712aab0cfc51b08ebef8279
JeevanDobbala <jeevandobbala@gmail.com>
2026-08-04 22:59:53 +0530

    changes in Product Controller

--- src/main/java/com/pizzahut/pizzahutwebsite/controller/ProductController.java
+++ src/main/java/com/pizzahut/pizzahutwebsite/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,9 +33,13 @@ public class ProductController {
 	}
 
 	@GetMapping("/getProductById/{productId}")
-	public Optional<ProductEntity> getProductById(@PathVariable Long productId) {
+	public ResponseEntity<?> getProductById(@PathVariable Long productId) {
+		Optional<ProductEntity> pe = ps.getProductById(productId);
 
-		return ps.getProductById(productId);
+		if (pe.isPresent()) {
+			return ResponseEntity.ok(pe.get());
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No product found with the given productId");
 	}
 
 	@GetMapping("/getAllProducts")
