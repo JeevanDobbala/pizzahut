91d6973dadf53b9cd3e78ffcf57fe738587aa580
JeevanDobbala <jeevandobbala@gmail.com>
2026-09-24 00:32:41 +0530

    payment related methods are added

--- src/main/java/com/pizzahut/pizzahutwebsite/services/PaymentService.java
+++ src/main/java/com/pizzahut/pizzahutwebsite/services/PaymentService.java
@@ -1,8 +1,32 @@
 package com.pizzahut.pizzahutwebsite.services;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
-@Service	
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.pizzahut.pizzahutwebsite.entity.PaymentTypeEntity;
+
+@Service
 public class PaymentService {
+	public String addPaymentType() {
+
+		return "adding payment failed!";
+	}
+
+	public PaymentTypeEntity getPaymentTypeById(@PathVariable Long PaymentId) {
+
+		return null;
+	}
+	
+	public List<PaymentTypeEntity> getAllPaymentTypes(@PathVariable Long PaymentId) {
+
+		return null;
+	}
+	
+	public PaymentTypeEntity deletePaymentTypeById(@PathVariable Long PaymentId) {
+
+		return null;
+	}
 
 }
